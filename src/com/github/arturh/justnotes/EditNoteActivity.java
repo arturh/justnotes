a50ca4ce5d861fc36551e29fa9d570f119ed6588
Artur Honzawa <arturh@gmail.com>
2013-03-03 15:00:51 +0100

    Launch activity

---
+++ src/com/github/arturh/justnotes/EditNoteActivity.java
@@ -0,0 +1,13 @@
+package com.github.arturh.justnotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditNoteActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.editnote_layout);
+		
+	}
+}
--- src/com/github/arturh/justnotes/ListNotesActivity.java
+++ src/com/github/arturh/justnotes/ListNotesActivity.java
@@ -2,21 +2,35 @@ package com.github.arturh.justnotes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ListNotesActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listnotes_layout);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.listnotes, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		final int item_id = item.getItemId();
+		if (item_id == R.id.listnotes_menu_new) {
+			final Intent intent = new Intent(this, EditNoteActivity.class);
+			startActivity(intent);
+			return true;
+		} else {
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
