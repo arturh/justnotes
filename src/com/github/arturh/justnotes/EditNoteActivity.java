684d1cc665aa1e8be1f0af88499b66f84f3eb2f9
Artur Honzawa <arturh@gmail.com>
2013-03-03 15:13:35 +0100

    editnote wip

--- src/com/github/arturh/justnotes/EditNoteActivity.java
+++ src/com/github/arturh/justnotes/EditNoteActivity.java
@@ -2,12 +2,42 @@ package com.github.arturh.justnotes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-public class EditNoteActivity extends Activity {
+public class EditNoteActivity extends Activity implements
+		OnEditorActionListener {
+	private static final String EXTRA_NOTE_ID = "EXTRA_NOTE_ID";
+	
+	private EditText etNote;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editnote_layout);
-		
+
+		// find views
+		etNote = (EditText) findViewById(R.id.etNote);
+
+		// set listeners
+		etNote.setOnEditorActionListener(this);
+
+		// fetch data
+		final Bundle extras = getIntent().getExtras();
+		if (extras != null && extras.containsKey(EXTRA_NOTE_ID)) {
+			final long note_id = extras.getLong(EXTRA_NOTE_ID);
+		} else {
+			// new note
+		}
+
+		// load data
+	}
+
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
